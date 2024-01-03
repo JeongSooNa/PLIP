4b51904a75b9294c338b34011e67444eebd74157
JeongSoo Na <skwjdtn3171@naver.com>
2024-01-03 13:29:12 +0900

    feat js

--- get_PLIP_csv.py
+++ get_PLIP_csv.py
@@ -5,40 +5,20 @@
 
 ### Packages import
 import os
-import sys
 
+### 현재 경로 선언
+### python script를 실행하는 중 os.chdir()을 통해 경로 이동이 이뤄질 경우 절대 경로를 사용하지 않거나 위치에 의한 에러가 발생할 경우를 대비해 현재 경로를 선언해놓는다.
 current_path = os.getcwd()
 
-### parameter
-### Input data : Directory include input pdb files
-### Output directory : Directory saved PLIP output data and csv table
-input_dir = sys.argv[1]
+### report 상위 폴더(각 plip 결과)들을 모아놓은 파일 위치
+### 다음과 같이 경로 구성이 될 수 있도록 PLIP 돌릴 시 조정
+### input_dir / 각 output 폴더 / report.txt
+input_dir = ""
+
+### report.txt 가 들어있는 각 폴더 list 선언
 input_list = os.listdir(input_dir)
-output_dir = sys.argv[2]
+### list 갯수
 print("pdb number : " + str(len(input_list)))
-
-# 안돼 이동
-os.chdir(input_dir)
-
-### Make directory per pdb
-try:
-    if not os.path.exists(output_dir):
-        os.system("sudo mkdir " + output_dir)
-except OSError:
-    print("Error : Directory is already exists.")
-
-for i in input_list:
-    try:
-        if not os.path.exists(i):
-            os.system("sudo mkdir " + output_dir + "/" + i.replace(".pdb",""))
-    except OSError:
-        print("Error : Directory is already exists.")
-    
-
-### Run PLIP
-### command : sudo docker run --rm -v ${PWD}:/results -w /results -u $(id -u ${USER}):$(id -g ${USER}) pharmai/plip:latest -f target.pdb -yv -o output_dir/ -x -t -y -p
-for i in input_list:
-    os.system("sudo docker run --rm -v ${PWD}:/results -w /results -u $(id -u ${USER}):$(id -g ${USER}) pharmai/plip:latest -f " + i + " -yv -o " + output_dir + "/" + i.replace(".pdb","") + "/ -x -t -y -p")
 
 ### Report to csv table
 ### Interactions
@@ -51,25 +31,45 @@ for i in input_list:
 ### - Halogen Bond
 ### - Salt Bridge
 ### - Metal Complex
-f = open(output_dir + "/plip_value_table.csv")
+
+### csv file 생성
+f = open("plip_report.csv","w")
+### csv file의 header 입력
+### PDB이름, 위 주석과 같은 결합타입, 결합 위치 sequence number, 결합 위치 Amino-Acid sequence
+### Hydrogen Bonds의 경우 DIST가 H-A, D-A 두 개가 있기 때문에 DIST를 두개로 분리
 f.write("PDB,Interaction_type,RESNR,RESTYPE,DIST,DIST_sub\n")
+
+### csv file 작성을 위해 각 report.txt에 접근
 for i in range(0,len(input_list)):
-# for i in range(0,1):
-    r = open(output_dir + "/" + input_list[i].replace(".pdb","") + "/report.txt")
+    r = open(input_dir + "/" + input_list[i] + "/report.txt")
     report = r.read()
     
     # Hydrophobic Interactions
+    ### 해당 interaction이 있을 경우 실행
     if "**Hydrophobic Interactions**" in report:
+        ### 해당 interaction이 report.txt 내에 여러 개 있을 경우 모두 추출하기 위해 실행
         len = report.split("**Hydrophobic Interactions**\n")
+        
         for j in range(1,len):
+            ### report.txt 내의 표를 보고 이해 필요
+            ### tmp : 표의 첫 줄인 "+-------+---------+----------+-----------+"에서 "+" 사이의 "-" 들을 담은 list
+            ### report 별로 테이블의 크기가 다를 수 있으므로 길이를 파악하고 데이터를 추출하는 작업 필요
             tmp = report.split("**Hydrophobic Interactions**\n")[j].split("\n")[0].split("+")
+            
+            ### 추출한 테이블의 길이로 각 행이 들어있는 list(report_hi) 생성
+            ### "| 639   | PRO     | A        | 0         | UNK         | Z            | 3.90 | 23           | 3854          | 10.080, 48.240, -0.776 | 11.952, 47.829, 2.621 |"
+            ### 위와 같은 한 줄씩 list에 담겨있다.
             report_hi = report.split("+" + tmp[1].replace("-","=") + "+" + tmp[2].replace("-","=") + "+" + tmp[3].replace("-","=") + "+" + tmp[4].replace("-","=") + "+" + tmp[5].replace("-","=") + "+" + tmp[6].replace("-","=") + "+" + tmp[7].replace("-","=") + "+" + tmp[8].replace("-","=") + "+" + tmp[9].replace("-","=") + "+" + tmp[10].replace("-","=") + "+" + tmp[11].replace("-","=") + "+\n")[1].split("\n" + "+" + tmp[1] + "+" + tmp[2] + "+" + tmp[3] + "+" + tmp[4] + "+" + tmp[5] + "+" + tmp[6] + "+" + tmp[7] + "+" + tmp[8] + "+" + tmp[9] + "+" + tmp[10] + "+" + tmp[11] + "+" + "\n\n")[0].split("\n" + "+" + tmp[1] + "+" + tmp[2] + "+" + tmp[3] + "+" + tmp[4] + "+" + tmp[5] + "+" + tmp[6] + "+" + tmp[7] + "+" + tmp[8] + "+" + tmp[9] + "+" + tmp[10] + "+" + tmp[11] + "+")
+            
             # print(len(report_hi))
+            
             for k in range(0,len(report_hi)):
+                ### 각 행에서 필요한 컬럼 추출
                 RESNR = report_hi[k].split("|")[1].replace(" ","")
                 RESTYPE = report_hi[k].split("|")[2].replace(" ","")
                 DIST = report_hi[k].split("|")[7].replace(" ","")
-                ### updating
+                
+                ### 추출한 데이터를 csv file에 입력
                 f.write(input_list[i].replace(".pdb","") + ",Hydrophobic Interactions," + RESNR + "," + RESTYPE + "," + DIST + ",," + "\n")
             
     
@@ -88,7 +88,6 @@ for i in range(0,len(input_list)):
                 f.write(input_list[i].replace(".pdb","") + ",Hydrogen Bonds," + RESNR + "," + RESTYPE + "," + DIST + "," + DIST_sub + "\n")
             
     
-    
     # Salt Bridges
     if "**Salt Bridges**" in report:
         len = report.split("**Salt Bridges**\n")
@@ -102,10 +101,8 @@ for i in range(0,len(input_list)):
                 DIST = report_sb[k].split("|")[8].replace(" ","")
                 f.write(input_list[i].replace(".pdb","") + ",Salt Bridges," + RESNR + "," + RESTYPE + "," + DIST + ",," + "\n")
 
+### csv writing 종료
 f.close()
 
-### I will update the Calculation Process & Filtering System
-### Update code clearly
-### MIT License
-
+### 현재 경로로 복귀
 os.chdir(current_path)
